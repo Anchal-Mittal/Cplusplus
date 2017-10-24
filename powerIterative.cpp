c9e0a443bc45e9c4fcd2be8ebe1b99fa46c85b5c
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2017-10-25 03:30:19 +0530

    Update power function  compute in O(log n) time

--- powerIterative.cpp
+++ powerIterative.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
-void pow ( double , int);
+   //DEFINATION OF POWER FUNCTION
+ 
+ int pow(int num, int exp){
+   
+	if(exp = = 0)
+			  return 1;
+	int  temp = pow(num,exp/2);
+	if(exp % 2 = = 0)
+	      return (temp * temp);
+  else 
+	      return num * temp * temp; 
 
-int main() {
-    
-    double base ;
-    int power;
-    cout << "\n Enter Base \t : \t " ;
-    cin >> base;
-    if ( base == 0 ) {
-	cout << "\n Base can't be zero. \n" ;
-   	return 0;
-    }
-    cout << "\n Enter Power \t : \t " ;
-    cin >> power;
-    pow ( base , power ) ;
-    return 0;
 }
 
-void pow  (   double base , int power ) {
-    double temp = 1;
-    bool flag = false ;
-    if ( power < 0 ) {
- 	flag = true;
-	power += ( -1 ) * ( 2 ) * power ; 
-    }
-    for ( int i = power ; i > 0 ; --i ) {
-        temp *= base  ;
-    
-    }
-    if ( flag == true ) {
-   	temp = 1 / temp ;
-    }
-    cout << "\n Result is \t : \t " << temp  << "\n";   
+int main(){
+int num = 5;
+int exp = 3;
+int result = pow(5,3);	
+cout << "THE RESULT WILL be " << result;
+return 0;
 }
-
